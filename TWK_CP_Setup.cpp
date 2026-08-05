bdc3a2ed6e793c29dd4401f80e173250e8d592aa
Lê Đình Minh Quân <ledinhminhquan222010@gmail.com>
2026-08-05 13:54:05 +0700

    Create TWK_CP_Setup.cpp

---
+++ TWK_CP_Setup.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+//clean code competitive
+void file(string x){if(x.empty())return;freopen((x+".inp").c_str(),"r",stdin);freopen((x+".out").c_str(),"w",stdout);}
+void buffIO(bool tieCin=true,bool tieCout=true){ios_base::sync_with_stdio(false);if(tieCin)cin.tie(0);if(tieCout)cout.tie(0);}
+
+
+
+signed main() {
+    buffIO();
+    file("");
+
+}
