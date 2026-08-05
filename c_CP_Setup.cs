de9bfabe7bd05d68123884bc2104a466f32acbe3
Lê Đình Minh Quân <ledinhminhquan222010@gmail.com>
2026-08-05 14:38:49 +0700

    Update c_CP_Setup.cs

--- c_CP_Setup.cs
+++ c_CP_Setup.cs
@@ -27,7 +27,7 @@
 #include <threads.h>
 #include <uchar.h>
 //clean code competitive
-void file(const char *name) {if(name[0]=='\0') return;char in[100],out[100];sprintf(in,"%s.inp",name);sprintf(out,"%s.out",name);freopen(in,"r",stdin);freopen(out,"w",stdout);}
+void file(const char *name){if(name[0]=='\0')return;char in[100],out[100];sprintf(in,"%s.inp",name);sprintf(out,"%s.out",name);freopen(in,"r",stdin);freopen(out,"w",stdout);}
 
 
 
