c9925571a8e3a700aab85290faead00edffcd01b
Lê Đình Minh Quân <ledinhminhquan222010@gmail.com>
2026-08-05 14:20:04 +0700

    Create c#_CP_set.cs

---
+++ c#_CP_set.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+//clean code competitive
+class TWK {
+    static void buffIO(){Console.SetIn(new StreamReader(Console.OpenStandardInput()));Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));}
+    static void file(string x){if(x=="")return;Console.SetIn(new StreamReader(x+".inp"));Console.SetOut(new StreamWriter(x+".out"));}
+    
+    
+    
+    static void Main() {
+        buffIO();
+        file("");
+        //code here
+        
+        
+        
+        //end code
+        Console.Out.Flush();
+    }
+}
