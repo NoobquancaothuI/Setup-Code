ccf33d95257207e5bb91d4216b46ff098d8f32e7
Lê Đình Minh Quân <ledinhminhquan222010@gmail.com>
2026-09-21 17:31:16 +0700

    Update c++_CP_Setup.cpp

--- c++_CP_Setup.cpp
+++ c++_CP_Setup.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 //clean code competitive
 void file(string x){if(x.empty())return;freopen((x+".inp").c_str(),"r",stdin);freopen((x+".out").c_str(),"w",stdout);}
-void buffIO(bool tieCin=true,bool tieCout=true){ios_base::sync_with_stdio(false);if(tieCin)cin.tie(0);if(tieCout)cout.tie(0);}
+void buffIO(bool tieCin=1,bool tieCout=1){ios_base::sync_with_stdio(0);if(tieCin)cin.tie(0);if(tieCout)cout.tie(0);}
 
 
 
